7abbc4e2f5180a1781eee64415e8de9126d5036a
roma2207 <roma.rukh@gmail.com>
2019-07-09 12:53:03 +0200

    added file

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sumthreeandfive = 0;
+            for (int i = 0; i <= 999; i++){
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sumthreeandfive += i;
+                }
+            }
+            
+            
+            Console.WriteLine("Total = " + sumthreeandfive);
+            Console.ReadKey();
+
+        }
+    }
+}
